a7f6144d2651d54f88e45a6bb350d5311d5d4010
kioli <lorenzo@usabilla.com>
2018-10-08 14:32:33 +0200

    Update coroutines

--- app/src/main/java/kioli/myalgia/common/interactor/UseCaseInvoker.kt
+++ app/src/main/java/kioli/myalgia/common/interactor/UseCaseInvoker.kt
@@ -2,17 +2,15 @@ package kioli.myalgia.common.interactor
 
 import kioli.myalgia.common.error.MyError
 import kioli.myalgia.common.functional.Either
-import kotlinx.coroutines.experimental.CoroutineDispatcher
+import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.async
-import kotlinx.coroutines.experimental.launch
 
 class UseCaseInvoker(private val dispatcher: CoroutineDispatcher) : Invoker {
 
     override fun <Params, Type : Any> execute(useCase: UseCase<Params, Type>,
                                               params: Params,
                                               onResult: (Either<MyError, Type>) -> Unit) {
-        val job = async(dispatcher) { useCase.run(params) }
-        launch(UI) { onResult.invoke(job.await()) }
+        val job = GlobalScope.async(dispatcher, CoroutineStart.DEFAULT, { useCase.run(params) })
+        GlobalScope.launch(Dispatchers.Main, CoroutineStart.DEFAULT, { onResult.invoke(job.await()) })
     }
 }
